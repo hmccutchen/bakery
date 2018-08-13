b7c0224e2fca84243afd6a2536ba7a2f65df8178
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-08-13 13:00:39 -0400

    created basic nav and imported bootstrap

--- bake.rb
+++ bake.rb
@@ -2,8 +2,37 @@ require 'sinatra'
 
 
 
+
+
+
+
+
+
 get '/' do
 
 
-erb :home
+  erb :home
 end
+
+
+
+
+
+#A user has a first and last name
+
+# A user has an email address
+
+#a user can now sign in
+
+
+
+
+
+#links for cookies, muffins and cakes
+# each link will send the user to another page showing the differnt options for the specific item chosen
+#each item should have a fixed price attached
+#on hover or click each item has item descriptions
+#users can add items to a shopping cart
+#users can buy items
+#once user buys an item email gets sent to user thanking and showing them all items purched and total amount
+#should have a page showing how many of each item was sold, playing on popularity
